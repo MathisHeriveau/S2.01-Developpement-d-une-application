c3eb110978ca839fa3a9add6b0a169c33550b056
Colas NAUDI <colasnaudi@gmail.com>
2022-06-09 12:08:40 +0200

    Version 0

---
+++ V0/V0/V0_Chifoumi.pro
@@ -0,0 +1,11 @@
+TEMPLATE = app
+CONFIG += console c++11
+CONFIG -= app_bundle
+CONFIG -= qt
+
+SOURCES += \
+    chifoumi.cpp \
+    main.cpp
+
+HEADERS += \
+    chifoumi.h
---
+++ V0/V0/chifoumi.cpp
@@ -0,0 +1,175 @@
+/**
+ * @file chifoumi.cpp
+ * @author Mathis HERIVEAU, Arthur LE MENN, Colas NAUDI
+ * @brief 
+ * @version 0.1
+ * @date 30-03-2022
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+**/
+
+#include "chifoumi.h"
+
+#include <cstdlib>
+#include <ctime>
+
+
+    ///* ---- PARTIE MOD�LE ---------------------------
+
+Chifoumi::Chifoumi()
+{
+    //ctor
+    // partie mod�le
+    initScores();
+    initCoups();
+    srand(time(NULL));
+}
+
+Chifoumi::~Chifoumi()
+{
+    //dtor
+}
+
+    /// Getters
+
+Chifoumi::UnCoup Chifoumi::getCoupJoueur() {
+	return coupJoueur;
+}
+
+Chifoumi::UnCoup Chifoumi::getCoupMachine() {
+    return coupMachine;
+}
+
+unsigned int Chifoumi::getScoreJoueur() {
+    return scoreJoueur;
+}
+
+unsigned int Chifoumi::getScoreMachine() {
+    return scoreMachine;
+}
+
+char Chifoumi::determinerGagnant(){
+    char gagnantARetourner;
+    // avant de commencer : match nul
+    gagnantARetourner = 'N';
+
+    // il sera modifi� dans l'un des cas suivants
+
+    // si le joueur a choisi pierre
+    if (getCoupJoueur() == pierre) {
+        // si la machine a choisi pierre
+        if (getCoupMachine() == pierre) {
+            // match nul
+            gagnantARetourner = 'N';
+        }
+        // si la machine a choisi papier
+        else if (getCoupMachine() == papier) {
+            // le joueur gagne
+            gagnantARetourner = 'M';
+        }
+        // si la machine a choisi ciseau
+        else if (getCoupMachine() == ciseau) {
+            // la machine gagne
+            gagnantARetourner = 'J';
+        }
+    }
+    // si le joueur a choisi papier
+    else if (getCoupJoueur() == papier) {
+        // si la machine a choisi pierre
+        if (getCoupMachine() == pierre) {
+            // la machine gagne
+            gagnantARetourner = 'J';
+        }
+        // si la machine a choisi papier
+        else if (getCoupMachine() == papier) {
+            // match nul
+            gagnantARetourner = 'N';
+        }
+        // si la machine a choisi ciseau
+        else if (getCoupMachine() == ciseau) {
+            // le joueur gagne
+            gagnantARetourner = 'M';
+        }
+    }
+    // si le joueur a choisi ciseau
+    else if (getCoupJoueur() == ciseau) {
+        // si la machine a choisi pierre
+        if (getCoupMachine() == pierre) {
+            // le joueur gagne
+            gagnantARetourner = 'M';
+        }
+        // si la machine a choisi papier
+        else if (getCoupMachine() == papier) {
+            // la machine gagne
+            gagnantARetourner = 'J';
+        }
+        // si la machine a choisi ciseau
+        else if (getCoupMachine() == ciseau) {
+            // match nul
+            gagnantARetourner = 'N';
+        }
+    }
+
+    return gagnantARetourner;
+}
+
+    ///* M�thodes utilitaires du Mod�le
+
+int randMinMax(int min, int max){
+    /* pr�-condition : min<max ;
+       Le nbre al�atoire est compris entre [min, max[ */
+    return rand()%(max-min) + min;
+}
+
+Chifoumi::UnCoup Chifoumi::genererUnCoup()
+{
+    UnCoup valeurGeneree;   // valeur à retourner
+    unsigned int nbAleatoire;
+    nbAleatoire = randMinMax(1, 4);
+    switch (nbAleatoire) {
+        case 1 : valeurGeneree = Chifoumi::pierre;break;
+        case 2 : valeurGeneree = Chifoumi::papier;break;
+        case 3 : valeurGeneree = Chifoumi::ciseau;break;
+        default: break;
+    }
+    return valeurGeneree;
+}
+
+    /// Setters
+
+void Chifoumi::setCoupJoueur(UnCoup p_coup){
+    coupJoueur = p_coup;
+}
+
+void Chifoumi::setCoupMachine(UnCoup p_coup) {
+    coupMachine = p_coup;
+}
+
+void Chifoumi::setScoreJoueur(unsigned int p_score) {
+    scoreJoueur = p_score;
+}
+
+void Chifoumi::setScoreMachine(unsigned int p_score) {
+    scoreMachine = p_score;
+}
+
+void Chifoumi::majScores(char p_gagnant) {
+    switch(p_gagnant)
+    {
+        case 'M' : scoreMachine++; break;
+        case 'J' : scoreJoueur++; break;
+        case 'N' :
+        default : break;
+    }
+}
+
+void Chifoumi::initScores() {
+    setScoreMachine(0);
+    setScoreJoueur(0);
+}
+
+void Chifoumi::initCoups() {
+    setCoupJoueur(rien);
+    setCoupMachine(rien);
+}
